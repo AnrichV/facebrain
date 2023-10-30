bfdfa4d18fa9be19e39dd83c1d4e38fb0fad169d
AnrichV <130673189+AnrichV@users.noreply.github.com>
2023-10-30 16:09:46 +0200

    port fix

--- facebrain-api/server.js
+++ facebrain-api/server.js
@@ -14,8 +14,9 @@ const db = knex({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
     host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
     port: 5432,
+    user: process.env.DATABASE_USER,
+
     password: process.env.DATABASE_PW,
     database: process.env.DATABASE_DB,
   },
